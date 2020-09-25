05508c4f6a346058ea3a7def9404dc318f861b00
Alessandro Sassi <alex.95.saxo@gmail.com>
2020-09-25 15:31:54 +0200

    seguendo i primi passi della guida socket.io

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-var PORT = process.env.PORT || 5000;
-var express = require('express');
+let express = require('express');
 var app = express();
+var http = require('http').createServer(app);
 
-var http = require('http');
-var server = http.Server(app);
-
-app.use(express.static('client'));
-
-server.listen(PORT, function() {
-  console.log('Chat server running');
+app.get('/', (req, res) => {
+    res.send('<h1>Ciao</h1>');
 });
 
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
+http.listen(3000, () => {
+    console.log('listening on *:3000');
 });
